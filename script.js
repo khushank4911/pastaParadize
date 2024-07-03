04fe02d6917be263432e1655898e214053fcbbc6
Khushank Singh Panwar <114572864+khushank4911@users.noreply.github.com>
2024-07-03 15:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
